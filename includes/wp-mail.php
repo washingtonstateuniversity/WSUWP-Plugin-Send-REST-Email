5970ed712dbea572d689506064cee6c1cfbc18b4
Jeremy Felt <jeremy.felt@gmail.com>
2017-05-05 15:34:42 -0700

    Adjust spacing for wp_mail to meet PHPCS sniff

--- includes/wp-mail.php
+++ includes/wp-mail.php
@@ -1,184 +1,184 @@
 <?php
 
 if ( ! function_exists( 'wp_mail' ) ) :
-/**
- * Sends mail as a replacement drop-in for WordPress core's default
- * wp_mail() via a REST endpoint provided by REST Email Proxy.
- *
- * A true return value does not automatically mean that the user received the
- * email successfully. It just only means that the method used was able to
- * process the request without any errors.
- *
- * @since 0.0.1
- *
- * @param string|array $to          Array or comma-separated list of email addresses to send message.
- * @param string       $subject     Email subject
- * @param string       $message     Message contents
- * @param string|array $headers     Optional. Additional headers.
- * @param string|array $attachments Optional. Files to attach.
- * @return bool Whether the email contents were sent successfully.
- */
-function wp_mail( $to, $subject, $message, $headers = '', $attachments = array() ) {
 	/**
-	 * Filters the wp_mail() arguments.
+	 * Sends mail as a replacement drop-in for WordPress core's default
+	 * wp_mail() via a REST endpoint provided by REST Email Proxy.
 	 *
-	 * @since 2.2.0
+	 * A true return value does not automatically mean that the user received the
+	 * email successfully. It just only means that the method used was able to
+	 * process the request without any errors.
 	 *
-	 * @param array $args A compacted array of wp_mail() arguments, including the "to" email,
-	 *                    subject, message, headers, and attachments values.
+	 * @since 0.0.1
+	 *
+	 * @param string|array $to          Array or comma-separated list of email addresses to send message.
+	 * @param string       $subject     Email subject
+	 * @param string       $message     Message contents
+	 * @param string|array $headers     Optional. Additional headers.
+	 * @param string|array $attachments Optional. Files to attach.
+	 * @return bool Whether the email contents were sent successfully.
 	 */
-	$atts = apply_filters( 'wp_mail', compact( 'to', 'subject', 'message', 'headers' ) );
+	function wp_mail( $to, $subject, $message, $headers = '', $attachments = array() ) {
+		/**
+		 * Filters the wp_mail() arguments.
+		 *
+		 * @since 2.2.0
+		 *
+		 * @param array $args A compacted array of wp_mail() arguments, including the "to" email,
+		 *                    subject, message, headers, and attachments values.
+		 */
+		$atts = apply_filters( 'wp_mail', compact( 'to', 'subject', 'message', 'headers' ) );
 
-	if ( isset( $atts['to'] ) ) {
-		$to = $atts['to'];
-	}
-
-	if ( isset( $atts['subject'] ) ) {
-		$subject = $atts['subject'];
-	}
-
-	if ( isset( $atts['message'] ) ) {
-		$message = $atts['message'];
-	}
-
-	if ( isset( $atts['headers'] ) ) {
-		$headers = $atts['headers'];
-	}
-
-	// Headers
-	$cc = $bcc = $reply_to = array();
-
-	if ( empty( $headers ) ) {
-		$headers = array();
-	} else {
-		if ( ! is_array( $headers ) ) {
-			// Explode the headers out, so this function can take both
-			// string headers and an array of headers.
-			$tempheaders = explode( "\n", str_replace( "\r\n", "\n", $headers ) );
-		} else {
-			$tempheaders = $headers;
+		if ( isset( $atts['to'] ) ) {
+			$to = $atts['to'];
 		}
-		$headers = array();
 
-		// If it's actually got contents
-		if ( !empty( $tempheaders ) ) {
-			// Iterate through the raw headers
-			foreach ( (array) $tempheaders as $header ) {
-				if ( strpos($header, ':') === false ) {
-					if ( false !== stripos( $header, 'boundary=' ) ) {
-						$parts = preg_split('/boundary=/i', trim( $header ) );
-						$boundary = trim( str_replace( array( "'", '"' ), '', $parts[1] ) );
+		if ( isset( $atts['subject'] ) ) {
+			$subject = $atts['subject'];
+		}
+
+		if ( isset( $atts['message'] ) ) {
+			$message = $atts['message'];
+		}
+
+		if ( isset( $atts['headers'] ) ) {
+			$headers = $atts['headers'];
+		}
+
+		// Headers
+		$cc = $bcc = $reply_to = array();
+
+		if ( empty( $headers ) ) {
+			$headers = array();
+		} else {
+			if ( ! is_array( $headers ) ) {
+				// Explode the headers out, so this function can take both
+				// string headers and an array of headers.
+				$tempheaders = explode( "\n", str_replace( "\r\n", "\n", $headers ) );
+			} else {
+				$tempheaders = $headers;
+			}
+			$headers = array();
+
+			// If it's actually got contents
+			if ( !empty( $tempheaders ) ) {
+				// Iterate through the raw headers
+				foreach ( (array) $tempheaders as $header ) {
+					if ( strpos($header, ':') === false ) {
+						if ( false !== stripos( $header, 'boundary=' ) ) {
+							$parts = preg_split('/boundary=/i', trim( $header ) );
+							$boundary = trim( str_replace( array( "'", '"' ), '', $parts[1] ) );
+						}
+						continue;
 					}
-					continue;
-				}
-				// Explode them out
-				list( $name, $content ) = explode( ':', trim( $header ), 2 );
+					// Explode them out
+					list( $name, $content ) = explode( ':', trim( $header ), 2 );
 
-				// Cleanup crew
-				$name    = trim( $name    );
-				$content = trim( $content );
+					// Cleanup crew
+					$name    = trim( $name    );
+					$content = trim( $content );
 
-				switch ( strtolower( $name ) ) {
-					// Mainly for legacy -- process a From: header if it's there
-					case 'from':
-						$bracket_pos = strpos( $content, '<' );
-						if ( $bracket_pos !== false ) {
-							// Text before the bracketed email is the "From" name.
-							if ( $bracket_pos > 0 ) {
-								$from_name = substr( $content, 0, $bracket_pos - 1 );
-								$from_name = str_replace( '"', '', $from_name );
-								$from_name = trim( $from_name );
+					switch ( strtolower( $name ) ) {
+						// Mainly for legacy -- process a From: header if it's there
+						case 'from':
+							$bracket_pos = strpos( $content, '<' );
+							if ( $bracket_pos !== false ) {
+								// Text before the bracketed email is the "From" name.
+								if ( $bracket_pos > 0 ) {
+									$from_name = substr( $content, 0, $bracket_pos - 1 );
+									$from_name = str_replace( '"', '', $from_name );
+									$from_name = trim( $from_name );
+								}
+
+								$from_email = substr( $content, $bracket_pos + 1 );
+								$from_email = str_replace( '>', '', $from_email );
+								$from_email = trim( $from_email );
+
+								// Avoid setting an empty $from_email.
+							} elseif ( '' !== trim( $content ) ) {
+								$from_email = trim( $content );
 							}
+							break;
+						case 'content-type':
+							if ( strpos( $content, ';' ) !== false ) {
+								list( $type, $charset_content ) = explode( ';', $content );
+								$content_type = trim( $type );
+								if ( false !== stripos( $charset_content, 'charset=' ) ) {
+									$charset = trim( str_replace( array( 'charset=', '"' ), '', $charset_content ) );
+								} elseif ( false !== stripos( $charset_content, 'boundary=' ) ) {
+									$boundary = trim( str_replace( array( 'BOUNDARY=', 'boundary=', '"' ), '', $charset_content ) );
+									$charset = '';
+								}
 
-							$from_email = substr( $content, $bracket_pos + 1 );
-							$from_email = str_replace( '>', '', $from_email );
-							$from_email = trim( $from_email );
-
-							// Avoid setting an empty $from_email.
-						} elseif ( '' !== trim( $content ) ) {
-							$from_email = trim( $content );
-						}
-						break;
-					case 'content-type':
-						if ( strpos( $content, ';' ) !== false ) {
-							list( $type, $charset_content ) = explode( ';', $content );
-							$content_type = trim( $type );
-							if ( false !== stripos( $charset_content, 'charset=' ) ) {
-								$charset = trim( str_replace( array( 'charset=', '"' ), '', $charset_content ) );
-							} elseif ( false !== stripos( $charset_content, 'boundary=' ) ) {
-								$boundary = trim( str_replace( array( 'BOUNDARY=', 'boundary=', '"' ), '', $charset_content ) );
-								$charset = '';
+								// Avoid setting an empty $content_type.
+							} elseif ( '' !== trim( $content ) ) {
+								$content_type = trim( $content );
 							}
-
-							// Avoid setting an empty $content_type.
-						} elseif ( '' !== trim( $content ) ) {
-							$content_type = trim( $content );
-						}
-						break;
-					case 'cc':
-						$cc = array_merge( (array) $cc, explode( ',', $content ) );
-						break;
-					case 'bcc':
-						$bcc = array_merge( (array) $bcc, explode( ',', $content ) );
-						break;
-					case 'reply-to':
-						$reply_to = array_merge( (array) $reply_to, explode( ',', $content ) );
-						break;
-					default:
-						// Add it to our grand headers array
-						$headers[trim( $name )] = trim( $content );
-						break;
+							break;
+						case 'cc':
+							$cc = array_merge( (array) $cc, explode( ',', $content ) );
+							break;
+						case 'bcc':
+							$bcc = array_merge( (array) $bcc, explode( ',', $content ) );
+							break;
+						case 'reply-to':
+							$reply_to = array_merge( (array) $reply_to, explode( ',', $content ) );
+							break;
+						default:
+							// Add it to our grand headers array
+							$headers[trim( $name )] = trim( $content );
+							break;
+					}
 				}
 			}
 		}
-	}
 
-	// From email and name
-	// If we don't have a name from the input headers
-	if ( !isset( $from_name ) )
-		$from_name = 'WordPress';
+		// From email and name
+		// If we don't have a name from the input headers
+		if ( !isset( $from_name ) )
+			$from_name = 'WordPress';
 
-	if ( !isset( $from_email ) ) {
-		// Get the site domain and get rid of www.
-		$sitename = strtolower( $_SERVER['SERVER_NAME'] );
-		if ( substr( $sitename, 0, 4 ) == 'www.' ) {
-			$sitename = substr( $sitename, 4 );
+		if ( !isset( $from_email ) ) {
+			// Get the site domain and get rid of www.
+			$sitename = strtolower( $_SERVER['SERVER_NAME'] );
+			if ( substr( $sitename, 0, 4 ) == 'www.' ) {
+				$sitename = substr( $sitename, 4 );
+			}
+
+			$from_email = 'wordpress@' . $sitename;
 		}
 
-		$from_email = 'wordpress@' . $sitename;
+		/**
+		 * Filters the email address to send from.
+		 *
+		 * @since 2.2.0
+		 *
+		 * @param string $from_email Email address to send from.
+		 */
+		$from_email = apply_filters( 'wp_mail_from', $from_email );
+
+		/**
+		 * Filters the name to associate with the "from" email address.
+		 *
+		 * @since 2.3.0
+		 *
+		 * @param string $from_name Name associated with the "from" email address.
+		 */
+		$from_name = apply_filters( 'wp_mail_from_name', $from_name );
+
+		// Set destination addresses
+		if ( ! is_array( $to ) ) {
+			$to = explode( ',', $to );
+		}
+
+		$data = array(
+			'send_to' => $to[0], // Only support the first address now.
+			'reply_to' => $from_email,
+			'send_from_name' => $from_name,
+			'subject' => $subject,
+			'message' => $message,
+		);
+
+		return WSU\SendRestEmail\Request\mail( $data );
 	}
-
-	/**
-	 * Filters the email address to send from.
-	 *
-	 * @since 2.2.0
-	 *
-	 * @param string $from_email Email address to send from.
-	 */
-	$from_email = apply_filters( 'wp_mail_from', $from_email );
-
-	/**
-	 * Filters the name to associate with the "from" email address.
-	 *
-	 * @since 2.3.0
-	 *
-	 * @param string $from_name Name associated with the "from" email address.
-	 */
-	$from_name = apply_filters( 'wp_mail_from_name', $from_name );
-
-	// Set destination addresses
-	if ( ! is_array( $to ) ) {
-		$to = explode( ',', $to );
-	}
-
-	$data = array(
-		'send_to' => $to[0], // Only support the first address now.
-		'reply_to' => $from_email,
-		'send_from_name' => $from_name,
-		'subject' => $subject,
-		'message' => $message,
-	);
-
-	return WSU\SendRestEmail\Request\mail( $data );
-}
 endif;
