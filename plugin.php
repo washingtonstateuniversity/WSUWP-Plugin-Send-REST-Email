28c824eae1b4bc56e830c38696136693f9b6351f
Jeremy Felt <jeremy.felt@gmail.com>
2017-09-18 12:47:58 -0700

    Bump version 0.0.3

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WSUWP Send REST Email
-Version: 0.0.2
+Version: 0.0.3
 Description: Send email through REST Email Proxy
 Author: washingtonstateuniversity, jeremyfelt
 Author URI: https://web.wsu.edu/
